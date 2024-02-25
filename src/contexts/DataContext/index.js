67b09ff88dc4491af177a473016ee70b951841d5
David Changea <14ir155@gmail.com>
2024-02-25 11:02:23 +0100

    correction du style eventcard pour voir le titre et la date sur tous les événements

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,9 +19,12 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [last, setLast] = useState(null)
   const getData = useCallback(async () => {
     try {
-      setData(await api.loadData());
+      const loadedData = await api.loadData()
+      setData(loadedData)
+      setLast(loadedData.events[loadedData.events.length - 1])
     } catch (err) {
       setError(err);
     }
@@ -37,6 +40,7 @@ export const DataProvider = ({ children }) => {
       value={{
         data,
         error,
+        last
       }}
     >
       {children}
